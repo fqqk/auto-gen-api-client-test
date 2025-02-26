a9296a7602c0143221df5fc536450e11281e97cd
fqqk <fqqk.work68@gmail.com>
2025-02-26 10:56:02 +0900

    :wrench: remove title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -29,23 +29,6 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
         <div className="mt-8">
           <h2 className="text-xl font-bold">User Data</h2>
           <pre className="mt-4 p-4 bg-gray-100 dark:bg-gray-800 rounded">
